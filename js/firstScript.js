a6f1bc6cd3556a7825d204ec824fe88bf1c941fe
ricardobah <rickribeiro99@gmail.com>
2018-08-31 14:35:31 -0300

    Conditional

--- js/firstScript.js
+++ js/firstScript.js
@@ -1,6 +1,12 @@
 alert("Hello world from my first javascript file!");
-var a = prompt("what is your name?");
-alert("hello "+ a+"!");
+var name = prompt("what is your name?");
+if(name!="Ricardo Ribeiro"){
+
+    alert("hello "+name+"!");
+}else{
+    alert("Olá metre!");
+}
+console.log("veio olhar oq aq?");
  
 
 
