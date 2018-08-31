d114228c8a2598d83dbe8d46e87d117579a2eb85
ricardobah <rickribeiro99@gmail.com>
2018-08-31 15:10:28 -0300

    FRIDAY GOOD BY

--- js/firstScript.js
+++ js/firstScript.js
@@ -1,10 +1,12 @@
 alert("Hello world from my first javascript file!");
 var name = prompt("what is your name?");
-if(name!="Ricardo Ribeiro"){
-
-    alert("hello "+name+"!");
-}else{
+if(name=="Ricardo Ribeiro"){
     alert("Olá metre!");
+
+}else if(name =="Rafael Lopes"){
+    alert("Cai fora");
+}else{
+    alert("hello "+name+"!");
 }
 console.log("veio olhar oq aq?");
  
