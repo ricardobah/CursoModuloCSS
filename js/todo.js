3a77935168e094c6c62dfc0758c12c9b782eb85c
ricardobah <rickribeiro99@gmail.com>
2018-09-03 16:18:27 -0300

    todo list

---
+++ js/todo.js
@@ -0,0 +1,35 @@
+var lista = [null];
+var aux;
+
+function arrayRemove(arr, value) {
+
+    return arr.filter(function(ele){
+        console.log(ele + " "+value);
+        return ele != value;
+    });
+ 
+ }
+
+do{
+aux=prompt("Todo list\n1.Add\n2.Remove\n3.Print\n99.Exit");
+
+    if(aux=="1"){
+       
+       lista.push(prompt("Value to add: "));
+       
+    }else if(aux=="2"){
+   
+        lista=arrayRemove(lista, prompt("Value to remove: "));
+    }else if(aux=="3"){
+        if(lista==0){
+            alert("Lista vazia!");
+        }else{
+            
+            alert(lista);
+        }
+    }else if (aux=="99"){
+
+    }else{
+        alert("Invalid number!");
+    }
+}while(aux!="99");
