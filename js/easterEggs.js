38740c42e08d798ae541e4bac551dbebbee292ad
ricardobah <rickribeiro99@gmail.com>
2018-09-04 16:15:25 -0300

    finish event change color

--- js/easterEggs.js
+++ js/easterEggs.js
@@ -2,10 +2,14 @@ var headers = document.querySelectorAll("li");
 headers.forEach(
     function(a){
         a.addEventListener("click",function(){
-            if(a.style.bckground!="rgb(ff, 00, 00)"){
-                a.style.background="rgb(ff, 00, 00)";   
+           
+            if(a.style.backgroundColor!="rgb(255, 0, 0)"){
+                 
+                a.style.backgroundColor="rgb(ff, 00, 00)";   
+                a.setAttribute("style","background-color:#ff0000;");
             }else{
-                a.style.backgroud="rgb(00, 00, ff)"
+               
+                a.setAttribute("style","background-color:#00ff00;");
             }
         });        
 
