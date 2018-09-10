7c8ab4cbea8ad5c36757ae04dc190197128f10d4
ricardobah <rickribeiro99@gmail.com>
2018-09-10 13:15:09 -0300

    list one piece

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
-$(".hig").css("background","purple");
+
+var classe;
+var classe2;
+var classe3;
+$("div:first").css("background","red");
+
+$("button").click(function(){
+    $("li").css("background", "white");
+    classe="."+$("input").val();
+    classe2="#"+$("input").val();
+    classe3=$("input").val();
+    $(classe).css("background","purple");
+    $(classe2).css("background","purple");
+    $(classe3).css("background","purple");
+
+
+});
