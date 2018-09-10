4b0d670cbedf5acac3cce13be7f1e0dbf4f5617e
ricardobah <rickribeiro99@gmail.com>
2018-09-10 15:00:16 -0300

    add Jquery exercise button and return

--- js/selectors.js
+++ js/selectors.js
@@ -2,7 +2,7 @@
 var classe;
 var classe2;
 var classe3;
-$("div:first").css("background","red");
+//$("div:first").css("background","red");
 
 $("button").click(function(){
     $("li").css("background", "white");
@@ -12,6 +12,5 @@ $("button").click(function(){
     $(classe).css("background","purple");
     $(classe2).css("background","purple");
     $(classe3).css("background","purple");
-
 
 });
