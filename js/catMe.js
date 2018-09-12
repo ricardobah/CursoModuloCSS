1a3d530e529b9bade864840b63037420f50bd831
ricardobah <rickribeiro99@gmail.com>
2018-09-12 13:41:23 -0300

    express and faker module

--- js/catMe.js
+++ js/catMe.js
@@ -1,5 +1,15 @@
 var catMe = require("cat-me");
+var joke = require("knock-knock-jokes");
+var faker = require('faker');
 
-console.log(catMe('delighted'))
+console.log(catMe());
+console.log(joke());
 
-alert(catMe('nyan'));
+
+for(var i=0;i<10;i++){
+
+    var randomName = faker.name.findName(); // Rowan Nikolaus
+    var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+    var randomCard = faker.helpers.createCard(); // random contact card containing many properties
+    console.log(randomName +" "+randomEmail+" "+randomCard);
+}
